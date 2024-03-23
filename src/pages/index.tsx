65707a1dd4b183473096c562878ae0bd9dcdcea3
Zax-code <abouliatimz@yahoo.com>
2024-03-23 14:37:15 +0100

    Cleaning index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
-import Head from 'next/head';
 
-const Home: NextPage = () => {
+export default function Home(){
   return (
       <main>
-        <h1>
-          Welcome to <a href="">RainbowKit</a> + <a href="">wagmi</a> +{' '}
-          <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
       </main>
   );
 };
-
-export default Home;
