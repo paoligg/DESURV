4492a0927d39a404b2c58f345bac456ab1a7758f
Zax-code <abouliatimz@yahoo.com>
2024-03-24 00:42:23 +0100

    Add description + reward + company name

--- src/components/createSurveyComponent.tsx
+++ src/components/createSurveyComponent.tsx
@@ -1,13 +1,18 @@
-import { createRef, useEffect, useState,RefObject } from 'react';
+import { createRef, useEffect, useState, RefObject } from 'react';
 import SurveyQuestion from '@/types/surveyquestion';
 import { useAccount, useWriteContract } from 'wagmi';
-import { type WriteContractParameters } from '@wagmi/core'
+import { type WriteContractParameters } from '@wagmi/core';
 import Answers from './possibleAnswers';
 import { surveysContract } from '@/contracts';
 
 export default function CreateSurveyComponent() {
-    const [questions, setQuestions] = useState<{survey : SurveyQuestion, questionRef: RefObject<HTMLInputElement>} []>([]);
+    const [questions, setQuestions] = useState<
+        { survey: SurveyQuestion; questionRef: RefObject<HTMLInputElement> }[]
+    >([]);
     const [maxResponse, setMaxResponse] = useState<number>(0);
+    const [Reward, setReward] = useState<number>(0);
+    const [description, setDescription] = useState<string>('');
+    const [entreprise, setEntreprise] = useState<string>('');
     const account = useAccount();
     const { writeContract } = useWriteContract();
     const handleQuestionChange = (index: number, question: string) => {
@@ -22,16 +27,17 @@ export default function CreateSurveyComponent() {
         const newQuestions = [...questions];
         const newQuestion = {
             questionRef: createRef<HTMLInputElement>(),
-            survey : {
-            question: '',
-            possibleAnswers: []}
+            survey: {
+                question: '',
+                possibleAnswers: [],
+            },
         };
         newQuestions.push(newQuestion);
         setQuestions(newQuestions);
     };
 
     const handleSendSurvey = () => {
-        const args= [
+        const args = [
             questions
                 .map(
                     (question) =>
@@ -39,46 +45,48 @@ export default function CreateSurveyComponent() {
                 )
                 .join(';'),
             BigInt(maxResponse),
-            'description',
+            description,
             'public_key',
             BigInt(questions.length),
-            'SG'
+            entreprise,
         ];
-        const variables : WriteContractParameters = {
+        const variables: WriteContractParameters = {
             account: account.address,
             address: surveysContract.address,
             abi: surveysContract.abi,
             functionName: 'createSurvey',
             args,
-            maxFeePerBlobGas: BigInt(10000),
+            maxFeePerBlobGas: BigInt(Reward),
             blobs: [],
             value: BigInt(1000),
         };
-        const onSuccess =  (data: any) => {
+        const onSuccess = (data: any) => {
             console.log(data);
-        }
+        };
         const onError = (error: any) => {
             console.log(error);
-        }
+        };
         const onSettled = () => {
-            console.log("Transaction settled");
-        }
-        writeContract(variables,{
+            console.log('Transaction settled');
+        };
+        writeContract(variables, {
             onSuccess,
             onError,
-            onSettled
-        }
-        );
+            onSettled,
+        });
     };
 
     return (
-        <div className='flex flex-col gap-8 justify-center items-center'>
+        <div className="flex flex-col items-center justify-center gap-8">
             <h1>Create a survey</h1>
-            <div className="flex flex-row flex-wrap p-4 gap-8">
+            <div className="flex flex-row flex-wrap gap-8 p-4">
                 {questions.map((question, index) => (
-                    <div key={index} className="flex flex-col px-4 py-10 opacity-80 shadow-[20px_20px_20px_0px] shadow-black bg-red-500 rounded-3xl hover:bg-red-400">
+                    <div
+                        key={index}
+                        className="flex flex-col rounded-3xl bg-red-500 px-4 py-10 opacity-80 shadow-[20px_20px_20px_0px] shadow-black hover:bg-red-400"
+                    >
                         <input
-                            className="bg-transparent  border border-white text-center  p-2 text-xl rounded-md"
+                            className="rounded-md  border border-white bg-transparent  p-2 text-center text-xl"
                             ref={question.questionRef}
                             value={question.survey.question}
                             onChange={(event) =>
@@ -92,12 +100,46 @@ export default function CreateSurveyComponent() {
                         />
                     </div>
                 ))}
-                <button className='text-purple-800 font-bold text-xl' onClick={() => handleAddQuestion()}>
+                <button
+                    className="text-xl font-bold text-purple-800"
+                    onClick={() => handleAddQuestion()}
+                >
                     Add question
                 </button>
             </div>
-                <input className='rounded-2xl text-black border border-purple-800 py-2 px-4' type="number" value={maxResponse} onChange={(event) => setMaxResponse(parseInt(event.target.value))} />
-            <button className='rounded-2xl text-black border border-purple-800 p-2' onClick={() => handleSendSurvey()}>Send Survey</button>
+            <input
+                className="rounded-2xl border border-purple-800 px-4 py-2 text-black"
+                type="number"
+                value={maxResponse}
+                onChange={(event) =>
+                    setMaxResponse(parseInt(event.target.value))
+                }
+            />
+            <input
+                className="rounded-2xl border border-purple-800 px-4 py-2 text-black"
+                type="number"
+                value={Reward}
+                onChange={(event) => setReward(parseInt(event.target.value))}
+            />
+            <textarea
+                value={description}
+                className="rounded-2xl border border-purple-800 p-2 text-black"
+                placeholder="Description"
+                onChange={(event) => setDescription(event.target.value)}
+            />
+            <input
+                className="rounded-2xl border border-purple-800 p-2 text-black"
+                type="text"
+                value={entreprise}
+                placeholder="Entreprise"
+                onChange={(event) => setEntreprise(event.target.value)}
+            />
+            <button
+                className="rounded-2xl border border-purple-800 p-2 text-black"
+                onClick={() => handleSendSurvey()}
+            >
+                Send Survey
+            </button>
         </div>
     );
 }
