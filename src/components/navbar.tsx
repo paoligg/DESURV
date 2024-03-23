125c7dccb4b3690520bdee674e78d6312ae037c9
Zax-code <abouliatimz@yahoo.com>
2024-03-23 14:06:42 +0100

    Styling navbar justify between

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 function Navbar() {
   return (
-    <nav className="text-white h-32 px-4 flex items-center justify-start gap-4 fixed top-0 w-full z-10">
+    <nav className="text-white h-32 px-8 flex items-center justify-between gap-4 fixed top-0 w-full z-10">
             <a href="/" className="text-2xl font-bold">SIARaclette</a>
             <ConnectButton />
   </nav>
