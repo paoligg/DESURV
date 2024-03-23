19ef8ed373e9832e4b2d0d08fa102e2a37c98220
jtsd <teyssedrejean@gmail.com>
2024-03-23 21:56:46 +0100

    Navbar change

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import myImage from '/public/Logo_Desurv.png';
+import Image from 'next/image'
+import Link from 'next/link';
 
 function Navbar() {
   return (
     <nav className="text-white h-32 px-8 flex items-center justify-between gap-4 fixed top-0 w-full z-10">
-            <a href="/" className="text-2xl font-bold">SIARaclette</a>
+                       
+      <a href="/">
+      <Image src={myImage} height="120" width="120" alt="Description" />
+      </a>
             <ConnectButton />
   </nav>
   );
