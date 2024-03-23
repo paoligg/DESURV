0085d546a7e98d5a99adc0e4b64e04489fc622b5
paoligg <blan.p@laposte.net>
2024-03-23 11:33:50 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-
+import { defineChain } from 'viem'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import {
@@ -15,16 +15,23 @@ import {
 } from 'wagmi/chains';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
+export const etherlink = defineChain({
+  id: 128123,
+  name: 'Etherlink Testnet',
+  nativeCurrency: { name: 'Tez', symbol: 'XTZ', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://node.ghostnet.etherlink.com/'] },
+  },
+  blockExplorers: {
+    default: { name: 'Etherlink Testnet explorer', url: 'https://testnet-explorer.etherlink.com/' },
+  },
+})
+
 const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    zora,
+    etherlink,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
   ssr: true,
