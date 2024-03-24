e42dbfeec79fa8b32af1003e0a9a13c1669188e3
Zax-code <abouliatimz@yahoo.com>
2024-03-24 10:18:30 +0100

    Footer more beautiful

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,21 +2,19 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="relative bottom-0 py-4 text-center">
-            <p>
-                © 2024 DESURV Open-Source Project H-W3B Hackathon
-                <a
-                    href="https://github.com/paoligg/SiaRaclette_Etherlink"
-                    className="flex items-center"
-                >
-                    <img
-                        src="/github.png"
-                        height="50"
-                        width="50"
-                        style={{ margin: 'auto' }}
-                    />
-                </a>
-            </p>
+        <footer className="relative bottom-1 mt-8 flex items-center justify-center gap-10 py-4 text-center">
+            © 2024 DESURV Open-Source Project H-W3B Hackathon
+            <a
+                href="https://github.com/paoligg/SiaRaclette_Etherlink"
+                className="flex items-center"
+            >
+                <img
+                    src="/github.png"
+                    height="50"
+                    width="50"
+                    style={{ margin: 'auto' }}
+                />
+            </a>
         </footer>
     );
 };
